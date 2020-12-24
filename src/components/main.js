ac9c9ed3f0bb4d8ae110de45503c61b72c1d4624
Diana Maduan <dianamaduan@MacBook-Air-Diana.local>
2020-12-24 20:38:37 +0600

    add-search-function

--- src/components/main.js
+++ src/components/main.js
@@ -1,13 +1,52 @@
-import React,{useEffect} from 'react'
+import React,{useState} from 'react'
 import connect from 'react-redux/es/connect/connect'
 import {getWeather} from '../store/actions/weatherActions'
 function Main(props){
-    useEffect(()=>{
-        props.getWeather(524901)
-    },[])
+    
+    const {cityInfo} = props.weatherReducer
+    const [request,setRequest] = useState('')
     return(
         <div>
-            <h1 className='h1'>WEATHER APP</h1>
+            <h1 className='h1'>Поиск</h1>
+            <div className='row2'>
+                <input type='text' value={request} onChange={(e)=>setRequest(e.target.value)} placeholder='название города' className='search-input'></input>
+                <div className='search-btn' onClick={()=>props.getWeather(request)}><img src='/search.png' alt='search'></img></div>
+            </div>
+            
+            <h1 className='h1'>ПРОГНОЗ ПОГОДЫ - {cityInfo.name}</h1>
+            <div className='row'>
+                <div className='column item'>
+                    <h1>ОТЧЕТ О МЕСТНОЙ ПОГОДЕ</h1>
+                    <hr/>
+                    <div className='row'>
+                        <div className='column'>
+                            
+                            {cityInfo.weather && cityInfo.weather[0] && cityInfo.weather[0].main === 'Snow'?
+                            <img src='/sun_clouds.svg' alt='clouds'/>:
+                            cityInfo.weather && cityInfo.weather[0] && cityInfo.weather[0].main === 'Clouds' ?
+                            <img src='/clouds.svg' alt='clouds'/>:
+                            <img src='/sun.svg' alt='clouds'/>}
+                            <p>{cityInfo && cityInfo.wind ? cityInfo.wind.speed: '12'} км/час</p>
+                        </div>
+                        <div className='column'>
+                            <h1>CЕГОДНЯ</h1>
+                            <h2>{cityInfo.weather && cityInfo.weather[0] ? cityInfo.weather[0].description : 'text'}</h2>
+                            <h2>{cityInfo && cityInfo.main ? parseInt(cityInfo.main.temp) - 273 : 0} C</h2>
+                            <h2>{cityInfo && cityInfo.main ? parseInt(cityInfo.main.temp) - 469.57 : 0} F</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className='column item'>
+                    <h1>МОРСКОЙ ПРОГНОЗ</h1>
+                    <hr/>
+                    <p>СКОРОСТЬ ВЕТРА . . . . . . {cityInfo && cityInfo.wind ? cityInfo.wind.speed: '0'} км/час</p>
+                    <p>НАПРАВЛЕНИЕ ВЕТРА . . . . . . {cityInfo && cityInfo.wind ? cityInfo.wind.deg: '12'} градуса</p>
+                    <p>АТМОСФЕРНОЕ ДАВЛЕНИЕ . . . . . . {cityInfo && cityInfo.main ? cityInfo.main.pressure: '0'} </p>
+                    <p>MAX - ТЕМПЕРАТУРА . . . . . . {cityInfo && cityInfo.main ? parseInt(cityInfo.main.temp_max) - 273 : 0} C</p>
+                    <p>MIN - ТЕМПЕРАТУРА . . . . . . {cityInfo && cityInfo.main ? parseInt(cityInfo.main.temp_min) - 273 : 0} C</p>
+                    <p>ВЛАЖНОСТЬ . . . . . . {cityInfo && cityInfo.main ? cityInfo.main.humidity: '0'}</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/store/actions/weatherActions.js
+++ src/store/actions/weatherActions.js
@@ -1,8 +1,9 @@
 import {GET_ERROR,GET_INFO} from './types'
 import axios from 'axios/index'
 var apiKey = 'c05c1e37c38ab839523ad145494f7269'
-export const getWeather = (id) => dispatch => {
-    axios.get('http://api.openweathermap.org/data/2.5/weather?id='+id+'&lang=ru&appid='+apiKey)
+
+export const getWeather = (name) => dispatch => {
+    axios.get('http://api.openweathermap.org/data/2.5/weather?q='+name+'&lang=ru&appid='+apiKey)
     .then(response => {
         return dispatch ({
             type:GET_INFO,
